ad5a821bc8b222efdfb4670e5ffef27acd8cce09
Doc Ritezel <pair+doc@ministryofvelocity.com>
2022-07-19 13:46:58 -0700

    Participants see alerts dismiss after a delay
    [finishes #182742525]

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,10 @@
 import { Application } from "@hotwired/stimulus"
 import Reveal from 'stimulus-reveal-controller'
+import Notification from 'stimulus-notification'
 
 const application = Application.start()
 application.register('reveal', Reveal)
+application.register('notification', Notification)
 
 // Configure Stimulus development experience
 application.debug = false
