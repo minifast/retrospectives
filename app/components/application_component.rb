ec14620fe1d777babba74683069d6813a9007b4a
Doc Ritezel <pair+doc@ministryofvelocity.com>
2022-08-05 18:02:47 -0700

    Guest does not see an edit Board button
    [fixes #182855977]

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,5 +1,11 @@
 class ApplicationComponent < ViewComponent::Base
   include Devise::Controllers::Helpers
+  include DeviseGuests::Controllers::Helpers
+  include Pundit::Authorization
   include Turbo::FramesHelper
   include Turbo::StreamsHelper
+
+  def pundit_user
+    current_or_guest_user
+  end
 end
