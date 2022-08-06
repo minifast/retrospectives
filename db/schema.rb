358ec8ea79059aeb0093ca3d300b736a25f43df0
Doc Ritezel <pair+doc@ministryofvelocity.com>
2022-08-05 18:34:58 -0700

    Facilitator sees a consistent Column order
    [fixes #182859304]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_28_172101) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_06_012249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_172101) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "column_order"
     t.index ["board_id", "name"], name: "index_columns_on_board_id_and_name", unique: true
   end
 
